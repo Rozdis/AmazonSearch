71ad0c2ebf815ebea01bd6908f114008584d0292
Rozdis <64463700+Rozdis@users.noreply.github.com>
2021-03-11 13:59:14 +0200

    Update TestToRecognizeBookInList.java

--- src/test/java/TestToRecognizeBookInList.java
+++ src/test/java/TestToRecognizeBookInList.java
@@ -22,11 +22,11 @@ public class TestToRecognizeBookInList {
 
     @BeforeTest
     public void openAndFindPage(){
-	    DesiredCapabilities cap = DesiredCapabilities.chrome();
-	cap.setBrowserName("chrome");
      System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver_linux");
 
         webDriver = new ChromeDriver();
+	DesiredCapabilities cap = DesiredCapabilities.chrome();
+	cap.setBrowserName("chrome");
         SearchPage searchPage = new SearchPage(webDriver);
         webDriver.navigate().to("https://www.amazon.com/");
 
