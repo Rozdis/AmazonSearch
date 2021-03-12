e5010fa2987c4241d26833143a8bbb6b87e74e58
Rozdis <nekton00@gmail.com>
2021-03-12 09:48:49 +0200

    ds

--- src/test/java/TestToRecognizeBookInList.java
+++ src/test/java/TestToRecognizeBookInList.java
@@ -22,7 +22,7 @@ public class TestToRecognizeBookInList {
     private List<Book> books = new ArrayList<>();
 
     @BeforeTest
-    public void openAndFindPage() throws InterruptedException {
+    public void openAndFindPage() {
         System.setProperty("webdriver.chrome.driver",  "src/main/resources/chromedriver_linux");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
@@ -48,7 +48,7 @@ public class TestToRecognizeBookInList {
         ResultListOfBooks bookPage = new ResultListOfBooks(webDriver);
         bookPage.satValuesToList(books);
         Book book = new BookDetail("https://www.amazon.com/Effective-Java-Joshua-Bloch/dp/0134685997/ref=sr_1_1?dchild=1&keywords=java&qid=1613558034&s=books&sr=1-1", webDriver).getBookFromPage();
-
+        System.out.println("jihihujhihuhihuhihiknkjnjknkjnkunkunkmnkubnkjbnkbnu");
         Assert.assertTrue(books.contains(book));
     }
 
